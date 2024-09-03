bfe244f7a5655bee03a14aa725d8869f0c0a157b
Khushi Jamloki <124756310+khushi1224@users.noreply.github.com>
2024-09-03 11:07:41 +0530

    CREATING BASIC SETUP AND ADDING REGISTER ROUTE TO FRONTEND

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+
+const app = express();
+require("dotenv").config();
+
+
+app.use(cors());
+
+app.use(express.json());
+
+
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log("Connected to MongoDB");
+}).catch((err)=>{
+    console.error("Error connecting to MongoDB",err);
+})
+
+
+
+
+
+
+
+
+
+const server = app.listen(process.env.PORT,(req,res)=>{
+    console.log(`Server running on port ${process.env.PORT}`);
+})
